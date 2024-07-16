4dba02f35d633d18cc7c6402b7711aed2bbf53ef
Jeremy <ogprago@gmail.com>
2024-07-15 21:52:33 -0400

    Moved main() as the last function

--- src/Main.py
+++ src/Main.py
@@ -1,5 +1,22 @@
 import math
 
+def mortgageCalc(loan, rate, term):
+    decimalInRate = float(rate / 100)
+    mortgage = float(loan * (decimalInRate / 12) * (math.pow(1 + (decimalInRate / 12), 12 * term) / (math.pow(1 + (decimalInRate /12), 12 * term) - 1)))
+    return mortgage
+
+def cdCalc(deposit, rate, years):
+    rate_decimal = rate / 100
+    ending_bal = deposit * math.pow(1 + rate_decimal / 365, 365 * years)
+    return ending_bal
+
+def presentValueCalc(monthlyPay, intRate, years):
+    decimalIntRate = intRate / 100
+    periodIntRate = decimalIntRate / 12
+    totalNumPeriods = years * 12
+    presentValue = monthlyPay * ((1 - (math.pow(1 + periodIntRate, -totalNumPeriods))) / periodIntRate)
+    return presentValue
+
 def main():
     print("Which calculator would you like to use?")
     print("\t1) Mortgage")
@@ -35,24 +52,4 @@ def main():
         print("Wrong command.")
 
 if __name__ == "__main__":
-    main()        
-
-    
-def mortgageCalc(loan, rate, term):
-    decimalInRate = float(rate / 100)
-    mortgage = float(loan * (decimalInRate / 12) * (math.pow(1 + (decimalInRate / 12), 12 * term) / (math.pow(1 + (decimalInRate /12), 12 * term) - 1)))
-    return mortgage
-
-def cdCalc(deposit, rate, years):
-    rate_decimal = rate / 100
-    ending_bal = deposit * math.pow(1 + rate_decimal / 365, 365 * years)
-    return ending_bal
-
-def presentValueCalc(monthlyPay, intRate, years):
-    decimalIntRate = intRate / 100
-    periodIntRate = decimalIntRate / 12
-    totalNumPeriods = years * 12
-    presentValue = monthlyPay * ((1 - (math.pow(1 + periodIntRate, -totalNumPeriods))) / periodIntRate)
-    return presentValue
-
-    
+    main()  
