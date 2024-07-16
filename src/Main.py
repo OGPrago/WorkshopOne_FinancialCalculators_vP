cfb6321dae1a4aec01266d29b23b50a5ba15f073
Jeremy <ogprago@gmail.com>
2024-07-15 21:57:55 -0400

    Created Calculators.py, and moved each calculator to it

--- src/Main.py
+++ src/Main.py
@@ -1,21 +1,4 @@
-import math
-
-def mortgageCalc(loan, rate, term):
-    decimalInRate = float(rate / 100)
-    mortgage = float(loan * (decimalInRate / 12) * (math.pow(1 + (decimalInRate / 12), 12 * term) / (math.pow(1 + (decimalInRate /12), 12 * term) - 1)))
-    return mortgage
-
-def cdCalc(deposit, rate, years):
-    rate_decimal = rate / 100
-    ending_bal = deposit * math.pow(1 + rate_decimal / 365, 365 * years)
-    return ending_bal
-
-def presentValueCalc(monthlyPay, intRate, years):
-    decimalIntRate = intRate / 100
-    periodIntRate = decimalIntRate / 12
-    totalNumPeriods = years * 12
-    presentValue = monthlyPay * ((1 - (math.pow(1 + periodIntRate, -totalNumPeriods))) / periodIntRate)
-    return presentValue
+from Calculators import mortgageCalc, cdCalc, presentValueCalc
 
 def main():
     print("Which calculator would you like to use?")
